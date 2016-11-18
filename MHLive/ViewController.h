3a78d40c3cd83ceedcdfcaac8068790a7d396e19
Mars Hou <houjianchao1204@gmail.com>
2016-11-18 18:39:07 +0800

    project init

---
+++ MHLive/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MHLive
+//
+//  Created by Mars on 18/11/2016.
+//  Copyright © 2016 Mars. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
